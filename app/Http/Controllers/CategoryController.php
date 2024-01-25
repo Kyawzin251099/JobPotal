e73c28f1f68411d95dbb335e767416b34077a101
Kyaw Zin Aung <kyawzinaung.186380@gmail.com>
2024-01-25 09:36:12 +0630

    nothing change

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-             public function index($id){
+    public function index($id){
         $jobs = Job::where('category_id',$id)->paginate(20);
         $categoryName = Category::where('id',$id)->first();
         return view ('jobs.job-category',compact('jobs','categoryName'));
